db5305ee883629610d333ca454e9a15d725446b3
Saikiran196 <71423958+Saikiran196@users.noreply.github.com>
2024-05-29 12:15:02 +0530

    Update airt_record.py

--- python/airt_record.py
+++ python/airt_record.py
@@ -54,7 +54,7 @@ while file_ctr < nfiles:
 
     # Make sure that the proper number of samples was read
     rc = sr.ret
-    assert rc == N, 'Error {}: {}'.format(rc, errToStr(rc))
+    assert rc == N, 'Error {}: {}'.format(rc, errToStr(rc))     # I am getting OverFlow Error due to this line and can't able to rectify it. Can anyone send the modified Code.
 
     # Write buffer to multiple files. Reshaping the rx_buffer allows for iteration
     for file_data in rx_buff.reshape(files_per_buffer, real_samples_per_file):
